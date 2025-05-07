6b14a4765438feeb504731ee40b4bb5fd8168e53
Dov98 <dovtheawesome@gmail.com>
2025-05-06 21:34:48 -0400

    Multiplier

---
+++ C#/5/Multiply.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    internal class Multiply
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the calculator. Enter your first digit you want to multiply:");
+            int dig1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Great, now enter the second digit:");
+            int dig2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nThe numbers you entered multiplied together = ");
+            Console.WriteLine(Multiplier(dig1,dig2));
+        }
+
+        static long Multiplier(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
