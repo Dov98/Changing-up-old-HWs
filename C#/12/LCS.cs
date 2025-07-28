1b917c4993ebf9bf5436c4f3aabecdc7d6e2c654
Dov98 <dovtheawesome@gmail.com>
2025-07-28 15:13:11 -0400

    Library Checkout System

---
+++ C#/12/LCS.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    //Was planning on adding more to the library checkout system, such as removing books from the list when someone has one,
+    //and making a library class that will print all the studets and their books, but had to move on for other reasons.
+    //Maybe will come back to this, but probs not.
+    internal class LCS
+    {
+    }
+
+    internal class Book
+    {
+        public string title;
+        public string author;
+        public string ID;
+        public string genre;
+
+        public string TheTitle
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string TheAuthor
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public string TheID
+        {
+            get { return ID; }
+            set { ID = value; }
+        }
+
+        public string TheGenre
+        {
+            get { return genre;  }
+            set { genre = value; }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{TheTitle}, {TheAuthor}, {TheID}, {TheGenre}");
+        }
+    }
+
+    internal class Student
+    {
+        //Has a list of borrowed books
+        public string name;
+        public string[] bookList;
+
+        public void Print()
+        {
+            Console.Write($"Name: {name}, Current books borrowed: ");
+            foreach (var book in bookList)
+            {if(book == null)
+                {
+                    continue;
+                }
+                Console.Write(book);
+            }
+        }
+    }
+
+    internal class Library
+    {
+        //Holds a list of borrrowed books and student
+    }
+}
---
+++ C#/12/LCSTester.cs
@@ -0,0 +1,42 @@
+﻿namespace _12
+{
+    internal class LCSTester
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book();
+            Book b2 = new Book();
+
+            b1.title = "Cloudy with a chance of meatballs";
+            b1.author = "Judy Barrett";
+            b1.ID = "JB123";
+            b1.genre = "Children";
+
+            b2.title = "Cat in the Hat";
+            b2.author = "Dr. Seuss";
+            b2.ID = "DS321";
+            b2.genre = "Children";
+
+            Book b3 = new Book();
+            b3.title = "Lord of the Rings";
+            b3.author = "John Ronald Reuel Tolkien";
+            b3.ID = "JRRT213";
+            b3.genre = "Fantasy";
+
+            b1.Print();
+            b2.Print();
+            b3.Print();
+
+
+            //Borrow Limit
+            int BL = 3;
+
+            Student s1 = new Student();
+            s1.name = "John Doe";
+            s1.bookList = new String[BL];
+            s1.bookList[0] = "DS321";
+
+            s1.Print();
+        }
+    }
+}
